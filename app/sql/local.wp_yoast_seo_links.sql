c226caa5a352fb3bc4d3ad6bafe410d47751d301
PepseeActus <christophe.sbastien@gmail.com>
2020-03-27 00:47:31 +0100

    rajouter le git ignore

--- app/sql/local.wp_yoast_seo_links.sql
+++
@@ -1,5 +0,0 @@
-/*!40101 SET NAMES binary*/;
-/*!40014 SET FOREIGN_KEY_CHECKS=0*/;
-/*!40103 SET TIME_ZONE='+00:00' */;
-INSERT INTO `wp_yoast_seo_links` VALUES
-(5,"https://fr.wikipedia.org/wiki/JmaX#cite_note-3",154,0,"external");
