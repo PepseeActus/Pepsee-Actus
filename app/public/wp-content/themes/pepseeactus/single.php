af24751f4c39a9c09b6fb246e104299de415caaf
PepseeActus <christophe.sbastien@gmail.com>
2020-11-20 12:37:06 +0100

    fix front

--- app/public/wp-content/themes/pepseeactus/single.php
+++ app/public/wp-content/themes/pepseeactus/single.php
@@ -18,6 +18,40 @@ while ( have_posts() ) : the_post(); ?>
 					<?php the_title( '<h3 class="single-title"><span>', '</span></h3>' ); ?>
 				</div>
 			<?php endif; ?>
+			<?php $posts = get_field('artistes_associes');
+			if ($posts): ?>
+				<div class="artist desktop-visually-hidden">
+					<h3 class="widget-title">Artiste<?= (count($posts) > 1) ? 's' : ''; ?></h3>
+					<div class="artist__container">
+						<?php foreach ($posts as $post) {
+							$badge = get_field('compte_verifie');
+							$twitter = get_field('twitter');
+							$facebook = get_field('facebook');
+							$instagram = get_field('instagram');
+							$youtube = get_field('youtube'); ?>
+							
+							<div class="artist__container-box">
+								<div class="artist-image">
+									<a href="<?= get_the_permalink($post); ?>">
+										<img src="<?= get_the_post_thumbnail_url($post, 'thumbnail'); ?>" alt="<?= get_the_title($post); ?>">
+									</a>
+								</div>
+								<div class="artist-info">
+									<a class="artist-title" href="<?= get_the_permalink($post); ?>"><?= get_the_title($post); ?></a>
+									<?php if ($badge) echo '<i class="fas fa-check-circle"></i>'; ?>
+									<ul>
+										<?= ($twitter) ? '<li><a href="'.$twitter.'" target="_blank"><i class="fab fa-twitter"></i></a></li>' : ''; ?>
+										<?= ($instagram) ? '<li><a href="'.$instagram.'" target="_blank"><i class="fab fa-instagram"></i></a></li>' : ''; ?>
+										<?= ($facebook) ? '<li><a href="'.$facebook.'" target="_blank"><i class="fab fa-facebook-f"></i></a></li>' : ''; ?>
+										<?= ($youtube) ? '<li><a href="'.$youtube.'" target="_blank"><i class="fab fa-youtube"></i></a></li>' : ''; ?>
+									</ul>
+								</div>
+							</div>
+						<?php } ?>
+						<?php wp_reset_postdata(); ?>
+					</div>
+				</div>
+			<?php endif; ?>
 			<div class="single-content">
 				<?php
 					if (has_post_format( 'video' )) { ?>
@@ -49,7 +83,7 @@ while ( have_posts() ) : the_post(); ?>
 			</div>
 			<div id="graphcomment"></div>
 		</article>
-		<aside class="col-12 col-md-3 padding-inside">
+		<aside class="col-12 col-md-3 padding-inside mobile-visually-hidden">
 			<?php $posts = get_field('artistes_associes');
 			if ($posts): ?>
 				<div class="artist">
@@ -84,7 +118,7 @@ while ( have_posts() ) : the_post(); ?>
 					</div>
 				</div>
 			<?php endif; ?>
-			<div class="instagram-feed mobile-visually-hidden">
+			<div class="instagram-feed">
 				<?= do_shortcode("[instagram-feed]"); ?>
 			</div>
 		</aside>
