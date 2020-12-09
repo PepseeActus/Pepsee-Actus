55d8ed7965ea3a4082b2456dbcc241ae7b46988a
PepseeActus <christophe.sbastien@gmail.com>
2020-12-09 23:04:39 +0100

    little fix

--- app/public/wp-content/themes/pepseeactus/page.php
+++ app/public/wp-content/themes/pepseeactus/page.php
@@ -17,19 +17,10 @@ get_header(); ?>
 <?php 
 while ( have_posts() ) : the_post();
 	$theParent = wp_get_post_parent_id(get_the_ID());
-
-	if ($theParent) : ?>
-		<div class="page-nav">
-			<p>
-				<a href="<?= get_permalink($theParent); ?>" class="home-link"><i class="fa fa-home" aria-hidden="true"></i> Retour sur <?= get_the_title($theParent); ?></a>
-				<span><?php the_title(); ?></span>
-			</p>
-		</div>
-	<?php endif; ?>
-	<?php the_title( '<h1 class="page-title">', '</h1>' ); ?>
-	<div class="page-content margin-outside">
-		<?php the_content(); ?>
-	</div>
+	the_title( '<h1 class="page-title">', '</h1>' ); ?>
+<div class="page-content margin-outside">
+	<?php the_content(); ?>
+</div>
 
 <?php endwhile; // End of the loop.
 
